7376c74e74d73c2a250bc96054111187ee1e74c6
Ren <rnchandramouli42@gmail.com>
2021-05-14 13:06:15 -0600

    created questions and answers, and added sections for both

---
+++ script.js
@@ -0,0 +1,36 @@
+const start = document.getElementById("startBtn");
+const time = document.getElementById("countdown");
+
+var questions = [
+  {
+    quote: "Commonly used data types DO NOT include:",
+    options: ["A. Strings", "B. Booleans", "C. Alerts", "D. Numbers"],
+    correct: "C. Alerts",
+  },
+  {
+    quote: "The conditions of an if/else statement are stored within:",
+    options: ["A. Quotes", "B. Curly brackets", "C. Parentheses", "D. Square Brackets"],
+    correct: "C. Parentheses",
+  },
+  {
+    quote: "Arrays in Javascript can be used to store:",
+    options: ["A. Numbers and Strings", "B. Other Arrays ", "C. Booleans", "D. All of the Above"],
+    correct: "D. All of the Above",
+  },
+  {
+    quote: "String Values must be enclosed within ____ when assigned to variables:",
+    options: ["A. Commas", "B. Curly Brackets ", "C. Quotes", "D. Parentheses"],
+    correct: "C. Quotes",
+  },
+  {
+    quote: "A very useful tool used during development and debugging for printing content to be a debugger is:",
+    options: ["A. Javascript", "B. Terminal/bash ", "C. For loops ", "D. Console log"],
+    correct: "B. Terminal/bash",
+  },
+];
+
+//function to start quiz
+function startQuiz() {}
+
+//function to contain results
+function results() {}
